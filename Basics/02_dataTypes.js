fc8468d4bbf39a6391356a54e7df99f076f3595b
Atul Kumar <112787992+Atul-08@users.noreply.github.com>
2025-12-26 04:16:05 +0000

    dataTypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+
+let name   = "atul"
+let age = 21
+let isLoggedIn = false
+let state ;
+let temprature = null;
+
+// number 2 to power 53
+// Bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined  => not initailised
+// symbol => unique
+
+//object
+
+console.log(typeof undefined )
+console.log(typeof null )
