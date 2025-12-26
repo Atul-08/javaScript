e795c64021df8a8f43cc4e99bfcc57f97091b8a3
Atul Kumar <112787992+Atul-08@users.noreply.github.com>
2025-12-26 04:06:05 +0000

    variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,9 @@
+
+const accountId = 1221
+let userName = "Atul Kumar"
+let accountEmail = "atulmzn01@gmail.com"
+var accountPassword = "21342" 
+
+accountCity = "muzaffarnagar"
+
+console.table([accountId, userName, accountEmail, accountPassword, accountCity] )
