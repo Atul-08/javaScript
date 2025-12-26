a7a6d1307f1ed175bf819aa8103f0aa4a79f19d3
Atul Kumar <112787992+Atul-08@users.noreply.github.com>
2025-12-26 04:41:49 +0000

    convertion operation

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let num = "22"
+
+console.log(typeof num);
+
+let valueInNum = Number(num);
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+// "22" => 22
+// "22abc" => NaN  --- datatype = number
+//  true => 1 ; false => 0 
+
+
+let isLoggedIn = "atul"
+
+let booleanISLoggedIn = Boolean(isLoggedIn)
+console.log(booleanISLoggedIn)
+
+//   1    => true ; 0 => false
+//   ""   => false
+// "atul" => true
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(someNumber);
