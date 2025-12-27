f524ddf4922511d0548f08728c62773b42a3de25
Atul Kumar <112787992+Atul-08@users.noreply.github.com>
2025-12-27 06:18:17 +0000

    numbers

---
+++ Basics/05_NumbersAndMaths.js
@@ -0,0 +1,27 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+
+const otherNumber = 23.899
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//-----------------maths-----------------------------
+
+console.log(Math.abs(-5))
+console.log(Math.round(5.6))
+console.log(Math.ceil(5.3))
+console.log(Math.floor(5.9))
+console.log(Math.min(4,8,2,5,7,1))
+console.log(Math.max(-5,9,3,2,6,4))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
