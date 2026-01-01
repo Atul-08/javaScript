1f0d0df678e367bde8e6064ddc1d90d5d4cc06cf
Atul Kumar <112787992+Atul-08@users.noreply.github.com>
2026-01-01 13:46:16 +0000

    scope

---
+++ 03_Basics/02_miniHoisting.js
@@ -0,0 +1,16 @@
+
+console.log(addone(3));
+
+
+function addone(num){
+    return num+1
+}
+
+
+// console.log(addtwo(3));      not valid
+
+const addtwo = function(num){
+    return num+2
+}
+
+console.log(addtwo(3));
